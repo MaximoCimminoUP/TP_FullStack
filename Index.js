3fab32c25d82ae45b541c7b3ba235363538fa15a
Maximo <cimminomaximo@gmail.com>
2024-03-20 21:10:57 -0300

    Commit tercera clase. Resolver errores y checkear en postman

--- Index.js
+++ Index.js
@@ -8,13 +8,23 @@ require('dotenv').config();
 const PORT = process.env.PORT; //|| 8050;
 
 
-app.get("/", (req,res) =>{
-res.send("Todos putos2")
+
+app.get("/categorias", (req,res) =>{
+res.send("x");
 })
-app.post("/", (req,res) =>{
-    res.send("Todos_putos.post")
+app.get("/remeras",(req,res) => {
+    let resultado = {'Garfield:':{'precio':15200, 'talles': 's-XL', 'categoria': 'personajes' },
+                    'Snoopy': {'precio':14500, 'talles': 'XS - L', 'categoria':'personajes'
+                    }}
+                    res.json({'remeras':resultado})//retorna al frontend el json con la respuesta
+})
+app.post("/remeras", (req,res) =>{
+    let datos = req.body
+    console.log(req.params)
+    res.send("Llamada post.")
     })
-http.listen(PORT, () => {
+
+    http.listen(PORT, () => {
     console.log(`buenas, server corriendo en el port ${PORT}`);
  
 });
