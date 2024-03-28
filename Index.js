7e68c8e1bfd4cdfea9edb8c9711a3d7051888b96
MaximoCimminoUP <cimminomaximo@gmail.com>
2024-03-27 21:10:08 -0300

    clase 4, mongodb agregado, estructura agregada

--- Index.js
+++ Index.js
@@ -4,20 +4,54 @@ const express = require("express"); //importando el modulo de express
 const app = express(); //generando la instancia del modulo
 const http = require("http").createServer(app);
 require('dotenv').config();
-
 const PORT = process.env.PORT; //|| 8050;
 
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = "mongodb+srv://cimminomaximo1:PVsujzr1VRRixMF9@cluster0.bwqyzg4.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"; 
+//pasar uri al .env cuando este en casa
 
-app.get("/", (req,res) =>{
-res.send("Todos putos2")
+
+/****** *
+const UsrController;
+const ...Controller;
+*/
+app.get("/home", (req,res) =>{
+res.send("Hola")
 })
+app.get("/Categorias", (req,res) =>{
+    let resultado = 
+    })
 app.post("/", (req,res) =>{
-    res.send("Todos_putos.post")
+    res.send("Hola todos")
     })
 http.listen(PORT, () => {
     console.log(`buenas, server corriendo en el port ${PORT}`);
  
 });
+
+
+
+const client = new MongoClient(uri, {
+    serverApi: {
+      version: ServerApiVersion.v1,
+      strict: true,
+      deprecationErrors: true,
+    }
+  });
+  
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    await client.close();
+  }
+}
+run().catch(console.dir);
 
 
 
